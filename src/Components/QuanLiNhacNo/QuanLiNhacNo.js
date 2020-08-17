40bfa90ba3a8bb01d7c86cd6c104e9b88581df59
hhpr98 <39483775+hhpr98@users.noreply.github.com>
2020-08-18 00:25:50 +0700

    fix: remove dashboard of employee/admin

---
+++ src/Components/QuanLiNhacNo/QuanLiNhacNo.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import {
+    Table,
+    Input,
+    InputNumber,
+    Popconfirm,
+    Form,
+    Layout,
+    Col,
+    Row,
+    Typography,
+    Button,
+    Modal,
+    Radio,
+    Result
+} from 'antd';
+
+const { Content } = Layout;
+
+export default function DebtReminder() {
+    return (
+        <Content>
+
+        </Content>
+    )
+}
